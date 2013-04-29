2362a380825a82bec7c14010b739ab5fad97dedd
Dian Pratama Putra <dragonballdyanuin@gmail.com>
2013-04-29 18:43:48 +0700

    Commit Pertama
    Testing

---
+++ inc_ub/home.php
@@ -0,0 +1,79 @@
+    <div class="bg-top">
+        	<div class="bg-top-2">
+                <div class="bg">
+                    <div class="bg-top-shadow">
+                        <div class="main">
+                            <div class="gallery p3">
+                            	<div class="wrapper indent-bot">
+                                    <div id="gallery" class="content">
+                                       <div class="wrapper">
+                                           <div class="slideshow-container">
+                                                <div id="slideshow" class="slideshow"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div id="thumbs" class="navigation">
+                                        <ul class="thumbs noscript">
+                                            <li>
+                                                <a class="thumb" href="images/pariwisata/gili trawangan.jpg" title=""> <img src="images/thumb/gili trawangan.jpg" alt="" /><span></span> </a>
+                                            </li> 
+                                            <li>
+                                                <a class="thumb" href="images/pariwisata/pantai.jpg" title=""> <img src="images/thumb/pantai.jpg" alt="" /> <span></span></a>
+                                            </li> 
+                                            <li>
+                                                <a class="thumb" href="images/pariwisata/pantai pink.jpg" title=""> <img src="images/thumb/pantai pink.jpg" alt="" /> <span></span></a>
+                                            </li> 
+                                            <li>
+                                                <a class="thumb" href="images/pariwisata/pantai senggigi.jpg" title=""> <img src="images/thumb/pantai senggigi.jpg" alt="" /> <span></span></a>
+                                            </li> 
+                                            <li>
+                                                <a class="thumb" href="images/pariwisata/tanjung ringgit.jpg" title=""> <img src="images/thumb/tanjung ringgit.jpg" alt="" /> <span></span></a>
+                                            </li> 
+                                            <li>
+                                                <a class="thumb" href="images/pariwisata/pantai mawun.jpg" title=""> <img src="images/thumb/pantai mawun.jpg" alt="" /> <span></span></a>
+                                            </li>           
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="inner">
+                                    <div class="wrapper">
+                                        <span class="title img-indent3"><a href="admin web">HELLO!</a></span>
+                                        <div class="extra-wrap indent-top2">
+                                        	<strong>Lombok</strong> is an island in the West Nusa Tenggara province of Indonesia. It is part of the chain of the Lesser Sunda Islands, with the Lombok Strait separating it from Bali to the west and the Alas Strait between it and Sumbawa to the east.
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>	
+        </div>
+        <div class="bg-bot">
+        	<div class="main">
+            	<div class="container_12">
+                	<div class="wrapper">
+                    	<article class="grid_4">
+                        	<h3 class="prev-indent-bot">About Us</h3>
+                            <p class="prev-indent-bot">This <a target="_blank" href="http://blog.templatemonster.com/2011/08/22/free-website-template-clean-style-interior/ ">Interior Design Template</a> goes with two pack ages: with PSD source files and without them.</p>
+                            PSD source files are available for free for the registered members of Templates.com. The basic package (without PSD source) is available for anyone without registration.
+                        </article>
+                        <article class="grid_4">
+                        	<h3 class="prev-indent-bot">Testimonials</h3>
+                            <div class="quote">
+                            	<p class="prev-indent-bot">At vero eos et accusamus et iusto odio tium voluptatum deleniti atque corrupti quos<br> dolores et quas molestias excepturi sint occaecati cupiditate.</p>
+                                <h5>James Reese</h5>
+                                Managing Director
+                            </div>
+                        </article>
+                        <article class="grid_4">
+                        	<h3 class="prev-indent-bot">What’s New?</h3>
+                            <time class="tdate-1" datetime="2011-08-15"><a class="link" href="#">15.08.2011</a></time>
+                            <p class="prev-indent-bot">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque.</p>
+                            <time class="tdate-1" datetime="2011-08-11"><a class="link" href="#">11.08.2011</a></time>
+                            Totam rem aperiam, eaque ipsa quae ab illo inven tore veritatis et quasi architecto.
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </div>
