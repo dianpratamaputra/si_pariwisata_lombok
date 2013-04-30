0238ad7b970e19e90c3c661ac5ee348912ba10dd
Dian Pratama Putra <dragonballdyanuin@gmail.com>
2013-04-30 19:38:06 +0700

    Revert "Commit Pertama"
    This reverts commit 2362a380825a82bec7c14010b739ab5fad97dedd.

--- admin web/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_destroy();
-echo "<meta http-equiv='refresh' content='1 URL=index.php'>";
-echo "<p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
-		<p align=\"center\">Please Wait...<br /><img src=images/loading.gif /></p>";
-?>
